c6eeafda97c2b2617239a53afa9d293c3dcdec3f
Rann Day <rannday@pm.me>
2021-08-30 13:12:58 -0400

    updated

--- uninstall.sh
+++ uninstall.sh
@@ -5,10 +5,7 @@ systemctl --user stop master.target
 systemctl --user disable oasis.target
 systemctl --user disable master.target
 
-rm -rf $HOME/game
+#rm -rf $HOME/game
 #rm -rf $HOME/.steam
-rm -rf $HOME/.config/systemd/user/oasis.target
-rm -rf $HOME/.config/systemd/user/oasis@.service
-rm -rf $HOME/.config/systemd/user/master.service
-
+rm -rf $HOME/.config/systemd
 systemctl --user daemon-reload
